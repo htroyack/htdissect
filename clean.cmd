28c4374ab952beaedcc8f2391d739f1ce0802a79
Henrique Troyack <henriquetroyack@hotmail.com>
2020-05-29 10:15:49 -0300

    Fixes build automation script filename.

--- clean.cmd
+++
@@ -1,7 +0,0 @@
-rd /s /q .vs
-rd /s /q Release
-rd /s /q Debug
-rd /s /q x64
-del *.vcxproj.user
-
-del htdissect.exe htdissect.obj dasm.obj dmain.obj ascii.obj
---
+++ clear.cmd
@@ -0,0 +1,7 @@
+rd /s /q .vs
+rd /s /q Release
+rd /s /q Debug
+rd /s /q x64
+del *.vcxproj.user
+
+del htdissect.exe htdissect.obj dasm.obj dmain.obj ascii.obj
